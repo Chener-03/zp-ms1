f9eaf53d512cf0c3b644fa79dbe4b864571c442d
chen <chen@chener.xyz>
2023-02-15 17:23:20 +0800

    1

--- user/src/main/java/xyz/chener/zp/zpusermodule/controller/MenuController.java
+++ user/src/main/java/xyz/chener/zp/zpusermodule/controller/MenuController.java
@@ -3,12 +3,13 @@ package xyz.chener.zp.zpusermodule.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import xyz.chener.zp.common.config.UnifiedReturn;
+import xyz.chener.zp.zpusermodule.entity.UiRouting;
 import xyz.chener.zp.zpusermodule.entity.dto.MenuNameDto;
 import xyz.chener.zp.zpusermodule.service.MenuService;
+import xyz.chener.zp.zpusermodule.service.UiRoutingService;
+import xyz.chener.zp.zpusermodule.service.impl.UiRoutingServiceImpl;
 
 import java.util.List;
 
@@ -26,9 +27,11 @@ import java.util.List;
 public class MenuController {
 
     private final MenuService menuService;
+    private final UiRoutingServiceImpl uiRoutingService;
 
-    public MenuController(MenuService menuService) {
+    public MenuController(MenuService menuService, UiRoutingServiceImpl uiRoutingService) {
         this.menuService = menuService;
+        this.uiRoutingService = uiRoutingService;
     }
 
 
@@ -37,5 +40,31 @@ public class MenuController {
     public List<MenuNameDto> getAllMenusName() {
         return menuService.getAllMenuName();
     }
+
+
+    @GetMapping("/getMenuInfo")
+    @PreAuthorize("hasAnyRole('menu_list_query')")
+    public UiRouting getMenuInfo(@RequestParam Integer id) {
+        return uiRoutingService.lambdaQuery().eq(UiRouting::getId, id).one();
+    }
+
+    @PostMapping("/saveMenuInfo")
+    @PreAuthorize("hasAnyRole('menu_list_query')")
+    public UiRouting saveMenuInfo(@ModelAttribute UiRouting uiRouting) {
+        return uiRoutingService.saveOrUpdate(uiRouting) ? uiRouting : null;
+    }
+
+    @PostMapping("/deleteMenuInfo")
+    @PreAuthorize("hasAnyRole('menu_list_query')")
+    public Boolean deleteMenuInfo(@RequestParam Integer id) {
+        return uiRoutingService.removeById(id);
+    }
+
+    @GetMapping("/getAllParentMenus")
+    @PreAuthorize("hasAnyRole('menu_list_query')")
+    public List<UiRouting> getAllParentMenus() {
+        return uiRoutingService.lambdaQuery().select(UiRouting::getId, UiRouting::getMetaTitle).list();
+    }
+
 
 }
