5d9ecec47851f4b440d774d82a1757c863ac7569
chen <chen@chener.xyz>
2023-02-16 17:15:13 +0800

    1

--- user/src/main/java/xyz/chener/zp/zpusermodule/service/impl/PermissionServiceImpl.java
+++ user/src/main/java/xyz/chener/zp/zpusermodule/service/impl/PermissionServiceImpl.java
@@ -4,7 +4,9 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 import xyz.chener.zp.zpusermodule.dao.PermissionDao;
 import xyz.chener.zp.zpusermodule.entity.Permission;
+import xyz.chener.zp.zpusermodule.entity.UiRouting;
 import xyz.chener.zp.zpusermodule.service.PermissionService;
+import xyz.chener.zp.zpusermodule.service.UiRoutingService;
 
 import java.util.List;
 
@@ -17,10 +19,24 @@ import java.util.List;
 @Service
 public class PermissionServiceImpl extends ServiceImpl<PermissionDao, Permission> implements PermissionService {
 
+    private final UiRoutingService uiRoutingService;
+
+    public PermissionServiceImpl(UiRoutingService uiRoutingService) {
+        this.uiRoutingService = uiRoutingService;
+    }
+
     @Override
     public void flushUiPermission() {
-        List<Permission> ui = this.lambdaQuery().likeRight(Permission::getPermissionEnName, "UI_").list();
-        System.out.println();
+        this.lambdaUpdate().likeRight(Permission::getPermissionEnName, "UI_").remove();
+        List<Permission> list = uiRoutingService.lambdaQuery().orderByAsc(UiRouting::getId)
+                .select(UiRouting::getMetaTitle, UiRouting::getName)
+                .list().stream().map(e -> {
+                    Permission permission = new Permission();
+                    permission.setPermissionEnName("UI_" + e.getName());
+                    permission.setPermissionChName(e.getMetaTitle());
+                    return permission;
+                }).toList();
+        this.saveBatch(list);
     }
 }
 
