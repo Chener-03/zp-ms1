fb97911caeda5e8fb84e320724ee9d35e26c5ea1
chen <chen@chener.xyz>
2023-02-23 20:06:44 +0800

    1

--- gateway/src/main/java/xyz/chener/zp/zpgateway/ZpGatewayApplication.java
+++ gateway/src/main/java/xyz/chener/zp/zpgateway/ZpGatewayApplication.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableFeignClients
 @RestController
 public class ZpGatewayApplication {
+    //
 
     public static void main(String[] args) {
         System.setProperty("csp.sentinel.log.output.type","console");
