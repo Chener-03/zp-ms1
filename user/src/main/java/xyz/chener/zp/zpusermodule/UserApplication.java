b5fca9ec15b89bec7a610791d541e7d144c551d9
chen <chen@chener.xyz>
2023-03-21 17:21:48 +0800

    1

--- user/src/main/java/xyz/chener/zp/zpusermodule/UserApplication.java
+++ user/src/main/java/xyz/chener/zp/zpusermodule/UserApplication.java
@@ -1,36 +1,15 @@
 package xyz.chener.zp.zpusermodule;
 
 import com.alibaba.cloud.sentinel.endpoint.SentinelEndpointAutoConfiguration;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.RemovalListener;
-import com.google.common.cache.RemovalNotification;
-import io.micrometer.core.instrument.Gauge;
-import io.micrometer.core.instrument.MeterRegistry;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.metrics.cache.CacheMetricsRegistrar;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchClientAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.interceptor.TransactionAspectSupport;
 import xyz.chener.zp.common.config.feign.loadbalance.NormalLoadBalanceAutoConfiguration;
-import xyz.chener.zp.common.utils.ObjectUtils;
-import xyz.chener.zp.zpusermodule.entity.UserBase;
-import xyz.chener.zp.zpusermodule.utils.Ip2RegUtils;
 
-import java.time.Duration;
-import java.util.Random;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Supplier;
 
 @SpringBootApplication(exclude = SentinelEndpointAutoConfiguration.class)
 @EnableFeignClients
