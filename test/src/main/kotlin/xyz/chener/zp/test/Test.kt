59555e47d6b8641503e07630c89d02d3b4a0734b
chenzp <chenzhipeng@kcwl.com>
2023-09-21 15:24:14 +0800

    1

---
+++ test/src/main/kotlin/xyz/chener/zp/test/Test.kt
@@ -0,0 +1,10 @@
+package xyz.chener.zp.test
+
+class Test {
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello World!")
+        }
+    }
+}
